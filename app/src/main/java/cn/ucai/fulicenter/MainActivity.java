56f61c9a1b611a314ecf970c3ad1bb112e9f4635
wuzhengxing <873094117@qq.com>
2017-01-10 17:46:43 +0800

    add butterknife

--- app/src/main/java/cn/ucai/fulicenter/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/MainActivity.java
@@ -1,65 +1,81 @@
 package cn.ucai.fulicenter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    RadioButton rbNewGood, rbBoutique, rbCart, rbCategory, rbPersonal;
-    int index,currentIndex;
+    //RadioButton rbNewGood, rbBoutique, rbCart, rbCategory, rbPersonal;
+    int index, currentIndex;
     RadioButton[] rbs;
+    @BindView(R.id.layout_new_good)
+    RadioButton layoutNewGood;
+    @BindView(R.id.layout_boutique)
+    RadioButton layoutBoutique;
+    @BindView(R.id.layout_category)
+    RadioButton layoutCategory;
+    @BindView(R.id.layout_cart)
+    RadioButton layoutCart;
+    @BindView(R.id.layout_personal_center)
+    RadioButton layoutPersonalCenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initView();
     }
 
     private void initView() {
-        rbNewGood= (RadioButton) findViewById(R.id.layout_new_good);
-        rbBoutique= (RadioButton) findViewById(R.id.layout_boutique);
-        rbCart= (RadioButton) findViewById(R.id.layout_cart);
-        rbCategory= (RadioButton) findViewById(R.id.layout_category);
-        rbPersonal= (RadioButton) findViewById(R.id.layout_personal_center);
-        rbs=new RadioButton[5];
-        rbs[0]=rbNewGood;
-        rbs[1]=rbBoutique;
-        rbs[2]=rbCategory;
-        rbs[3]=rbCart;
-        rbs[4]=rbPersonal;
+        /*rbNewGood = (RadioButton) findViewById(R.id.layout_new_good);
+        rbBoutique = (RadioButton) findViewById(R.id.layout_boutique);
+        rbCart = (RadioButton) findViewById(R.id.layout_cart);
+        rbCategory = (RadioButton) findViewById(R.id.layout_category);
+        rbPersonal = (RadioButton) findViewById(R.id.layout_personal_center);*/
+        rbs = new RadioButton[5];
+        rbs[0] = layoutNewGood;
+        rbs[1] = layoutBoutique;
+        rbs[2] = layoutCategory;
+        rbs[3] = layoutCart;
+        rbs[4] = layoutPersonalCenter;
     }
 
     public void onCheckedChange(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.layout_new_good:
-                index=0;
+                index = 0;
                 break;
             case R.id.layout_boutique:
-                index=1;
+                index = 1;
                 break;
             case R.id.layout_category:
-                index=2;
+                index = 2;
                 break;
             case R.id.layout_cart:
-                index=3;
+                index = 3;
                 break;
             case R.id.layout_personal_center:
-                index=4;
+                index = 4;
                 break;
         }
-        if(index!=currentIndex){
+        if (index != currentIndex) {
             setRadioStatus();
         }
     }
-    public void setRadioStatus(){
-        for(int i=0;i<rbs.length;i++){
-            if(i!=index){
+
+    public void setRadioStatus() {
+        for (int i = 0; i < rbs.length; i++) {
+            if (i != index) {
                 rbs[i].setChecked(false);
-            }else {
+            } else {
                 rbs[i].setChecked(true);
             }
         }
-        currentIndex=index;
+        currentIndex = index;
     }
 }
