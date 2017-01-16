a5bfee26a24e9f097a9ef477f4c90fbf536045dd
wuzhengxing <873094117@qq.com>
2017-01-16 20:02:26 +0800

    用户登录

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/LoginActivity.java
@@ -1,15 +1,33 @@
 package cn.ucai.fulicenter.controller.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.bean.User;
+import cn.ucai.fulicenter.model.net.IModelUser;
+import cn.ucai.fulicenter.model.net.ModelUser;
+import cn.ucai.fulicenter.model.net.OnCompletionListener;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
+import cn.ucai.fulicenter.model.utils.ResultUtils;
+import cn.ucai.fulicenter.model.utils.SharedPrefrenceUtils;
 import cn.ucai.fulicenter.view.MFGT;
 
 public class LoginActivity extends AppCompatActivity {
+    IModelUser model;
+    @BindView(R.id.etUserName)
+    EditText etUserName;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +36,71 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.btLogin, R.id.btRegisterFree})
+    @OnClick({R.id.ivBack, R.id.btLogin, R.id.btRegisterFree})
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.ivBack:
+                MFGT.finish(this);
+                break;
             case R.id.btLogin:
+                checkInput();
                 break;
             case R.id.btRegisterFree:
                 MFGT.gotoRegister(this);
                 break;
         }
     }
+
+    private void checkInput() {
+        String userName = etUserName.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(userName)) {
+            etUserName.setError(getString(R.string.user_name_connot_be_empty));
+            etUserName.requestFocus();
+        } else if (TextUtils.isEmpty(password)) {
+            etPassword.setError(getString(R.string.password_connot_be_empty));
+            etPassword.requestFocus();
+        } else {
+            login(userName, password);
+        }
+    }
+
+    private void login(final String userName, String password) {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage(getString(R.string.logining));
+        model = new ModelUser();
+        model.login(this, userName, password, new OnCompletionListener<String>() {
+            @Override
+            public void onSuccess(String str) {
+                if (str != null) {
+                    Result result = ResultUtils.getResultFromJson(str, User.class);
+                    if (result != null) {
+                        if (result.getRetData() != null) {
+                            User user= (User) result.getRetData();
+                            SharedPrefrenceUtils.getInstance(LoginActivity.this).saveUser(user.getMuserName());
+                            MFGT.finish(LoginActivity.this);
+                        } else {
+                            if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER) {
+                                CommonUtils.showLongToast(getString(R.string.login_fail_unknow_user));
+                            } else if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD) {
+                                CommonUtils.showLongToast(getString(R.string.login_fail_error_password));
+                            }
+                        }
+                    } else {
+                        CommonUtils.showLongToast(getString(R.string.login_fail));
+                    }
+                } else {
+                    CommonUtils.showLongToast(getString(R.string.login_fail));
+                }
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                dialog.dismiss();
+                CommonUtils.showLongToast(error);
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/cn/ucai/fulicenter/model/utils/SharedPrefrenceUtils.java
@@ -0,0 +1,36 @@
+package cn.ucai.fulicenter.model.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Created by Administrator on 2017/1/16.
+ */
+
+public class SharedPrefrenceUtils {
+    private static final String SHARE_PREFRENCES_NAME = "cn.ucai.fulicenter_user";
+    private static final String SHARE_PREFRENCES_USER_USERNAME = "cn.ucai.fulicenter_user_username";
+    private static SharedPrefrenceUtils instance;
+    private static SharedPreferences preferences;
+
+    public SharedPrefrenceUtils(Context context) {
+        preferences = context.getSharedPreferences(SHARE_PREFRENCES_NAME, context.MODE_PRIVATE);
+    }
+
+    public static SharedPrefrenceUtils getInstance(Context context) {
+        if (instance == null) {
+            instance = new SharedPrefrenceUtils(context);
+        }
+        return instance;
+    }
+
+    public static void saveUser(String userName) {
+        preferences.edit().putString(SHARE_PREFRENCES_USER_USERNAME, userName).commit();
+    }
+
+    public static String getUser() {
+        return preferences.getString(SHARE_PREFRENCES_USER_USERNAME, null);
+    }
+}
